ccaf535049e33755303848579800c7d9058b486e
letan-165 <2251160057@ut.edu.vn>
2025-09-08 12:58:40 +0700

    chore: config redis to cache

---
+++ src/main/java/com/app/booking/config/Redis/CacheTtlProperties.java
@@ -0,0 +1,19 @@
+package com.app.booking.config.Redis;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "custom-cache")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CacheTtlProperties {
+    Long defaultTtl = 300L;
+    Map<String, Long> ttls = new HashMap<>();
+}
