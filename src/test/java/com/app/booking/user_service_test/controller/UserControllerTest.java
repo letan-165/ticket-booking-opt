609d7be62052deb4996e668f8e68a19e8cfa9ef7
letan-165 <2251160057@ut.edu.vn>
2025-09-20 19:22:24 +0700

    feat: init controller test

---
+++ src/test/java/com/app/booking/user_service_test/controller/UserControllerTest.java
@@ -0,0 +1,69 @@
+package com.app.booking.user_service_test.controller;
+
+import com.app.booking.common.PageResponse;
+import com.app.booking.internal.user_service.controller.UserController;
+import com.app.booking.internal.user_service.dto.request.UserRequest;
+import com.app.booking.internal.user_service.dto.response.UserResponse;
+import com.app.booking.internal.user_service.service.UserService;
+import com.app.booking.model_mock.RequestMock;
+import com.app.booking.model_mock.ResponseMock;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockitoBean
+    UserService userService;
+
+    UserRequest userRequest;
+    UserResponse userResponse;
+    String userID;
+
+    @BeforeEach
+    void initData(){
+        userRequest = RequestMock.userMock();
+        userResponse = ResponseMock.userMock();
+        userID = userResponse.getId();
+    }
+
+    @Test
+    void findAll_success() throws Exception {
+        List<UserResponse> users = new ArrayList<>();
+        users.add(userResponse);
+        users.add(userResponse);
+        users.add(userResponse);
+        PageResponse<UserResponse> page = PageResponse.<UserResponse>builder()
+                .content(users)
+                .build();
+
+        when(userService.findAll(any(Pageable.class))).thenReturn(page);
+
+        mockMvc.perform(get("/users/public"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1000))
+                .andExpect(jsonPath("result.length()").value(3));
+    }
+}
